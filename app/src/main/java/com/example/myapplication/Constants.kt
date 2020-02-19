8118e933820f21d799a0dfa5894f16d1cdc0ae74
yeongyeon <yyjjang9@naver.com>
2020-02-19 23:23:36 +0900

    commit

--- app/src/main/java/com/example/myapplication/Constants.kt
+++ app/src/main/java/com/example/myapplication/Constants.kt
@@ -1,6 +1,4 @@
 package com.example.myapplication
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 val LOTTO_URL = "http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo="
 val MAX_LOTTO_NUM = 45u
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.StringBuilder
 import java.util.*
@@ -13,7 +15,6 @@ import kotlin.random.Random
 import kotlin.random.nextUInt
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +53,9 @@ class MainActivity : AppCompatActivity() {
                  Request.Method.GET
                 ,url
                 ,Response.Listener<String> { response ->
+                    val mapper = jacksonObjectMapper()
+                    val resultMap : HashMap<String, String> = mapper.readValue(response, object : TypeReference<HashMap<String, String>>() {})
+
                     winnings_textView.text = ""
                 }
                 ,Response.ErrorListener {
@@ -61,4 +65,6 @@ class MainActivity : AppCompatActivity() {
             queue.add(stringRequest)
         }
     }
+
+
 }
